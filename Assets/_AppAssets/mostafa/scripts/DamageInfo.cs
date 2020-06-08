9015655136a83e4e9ee65b84f09ff6fd7c8205f1
sirius707 <steave_mudblood@yahoo.com>
2020-06-08 15:17:42 +0200

    mostafa - damage

---
+++ Assets/_AppAssets/mostafa/scripts/DamageInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageInfo : MonoBehaviour
+{
+    public enum WeaponType { firearm, grenade, mine };
+    public enum DamageEffect { pan, fly, freeze, root, explotion };
+
+    public WeaponType type;
+    public DamageEffect effect;
+
+    public int source_id; //player id
+    public int team_id;//
+    public float damage_amount;
+    public float effect_duration;
+    public float radius;
+    public bool aoe;//are of effect\
+}
---
+++ Assets/_AppAssets/mostafa/scripts/GetDamaged.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetDamaged : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        DamageInfo dm = collision.GetComponent<DamageInfo>();
+
+        switch (dm.effect)
+        {
+            case DamageInfo.DamageEffect.freeze:
+                print("frozen");
+                break;
+
+        }
+    }
+}
